b165e8edae2a96ff6f02efddd0b72e699a76fe09
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-06-13 14:13:52 -0400

    Made Code Easier to Read

--- js/index.js
+++ js/index.js
@@ -1,43 +1,44 @@
 const siteContent = {
-  "nav": {
-    "nav-item-1": "Services",
-    "nav-item-2": "Product",
-    "nav-item-3": "Vision",
-    "nav-item-4": "Features",
-    "nav-item-5": "About",
-    "nav-item-6": "Contact",
-    "img-src": "img/logo.png"
-  },
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
-    "img-src": "img/header-img.png"
-  },
-  "main-content": {
-    "features-h4":"Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-  },
-  "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
-  },
-  "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
-  },
+    "nav": {
+        "nav-item-1": "Services",
+        "nav-item-2": "Product",
+        "nav-item-3": "Vision",
+        "nav-item-4": "Features",
+        "nav-item-5": "About",
+        "nav-item-6": "Contact",
+        "img-src": "img/logo.png"
+    },
+    "cta": {
+        "h1": "DOM Is Awesome",
+        "button": "Get Started",
+        "img-src": "img/header-img.png"
+    },
+    "main-content": {
+        "features-h4": "Features",
+        "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+        "about-h4": "About",
+        "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+        "middle-img-src": "img/mid-page-accent.jpg",
+        "services-h4": "Services",
+        "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+        "product-h4": "Product",
+        "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+        "vision-h4": "Vision",
+        "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    },
+    "contact": {
+        "contact-h4": "Contact",
+        "address": "123 Way 456 Street Somewhere, USA",
+        "phone": "1 (888) 888-8888",
+        "email": "sales@greatidea.io",
+    },
+    "footer": {
+        "copyright": "Copyright Great Idea! 2018"
+    },
 };
 
 // Example: Update the img src for the logo
+
 // Images
 const logo = document.getElementById("logo-img");
 logo.src = siteContent["nav"]["img-src"]
@@ -56,14 +57,45 @@ headers[1].textContent = siteContent["main-content"]["about-h4"]
 headers[2].textContent = siteContent["main-content"]["services-h4"]
 headers[3].textContent = siteContent["main-content"]["product-h4"]
 headers[4].textContent = siteContent["main-content"]["vision-h4"]
-headers[5].textContent = siteContent["main-content"]["contact-h4"]
-
+headers[5].textContent = siteContent["contact"]["contact-h4"]
 
 // Nav
-const navText1 = document.querySelector("nav a:nth-child(2)").textContent = siteContent["nav"]["nav-item-1"]
-const navText2 = document.querySelector("nav a:nth-child(3)").textContent = siteContent["nav"]["nav-item-2"]
-const navText3 = document.querySelector("nav a:nth-child(4)").textContent = siteContent["nav"]["nav-item-3"]
-const navText4 = document.querySelector("nav a:nth-child(5)").textContent = siteContent["nav"]["nav-item-4"]
-const navText5 = document.querySelector("nav a:nth-child(6)").textContent = siteContent["nav"]["nav-item-5"]
-const navText6 = document.querySelector("nav a:nth-child(7)").textContent = siteContent["nav"]["nav-item-6"]
+const links = document.querySelectorAll("a");
+links[0].textContent = siteContent["nav"]["nav-item-1"]
+links[1].textContent = siteContent["nav"]["nav-item-2"]
+links[2].textContent = siteContent["nav"]["nav-item-3"]
+links[3].textContent = siteContent["nav"]["nav-item-4"]
+links[4].textContent = siteContent["nav"]["nav-item-5"]
+links[5].textContent = siteContent["nav"]["nav-item-6"]
+links[0].style.color = "green";
+links[1].style.color = "green";
+links[2].style.color = "green";
+links[3].style.color = "green";
+links[4].style.color = "green";
+links[5].style.color = "green";
 
+// Paragraphs
+const paragraphs = document.querySelectorAll("p");
+paragraphs[0].textContent = siteContent["main-content"]["features-content"]
+paragraphs[1].textContent = siteContent["main-content"]["about-content"]
+paragraphs[2].textContent = siteContent["main-content"]["services-content"]
+paragraphs[3].textContent = siteContent["main-content"]["product-content"]
+paragraphs[4].textContent = siteContent["main-content"]["vision-content"]
+paragraphs[5].textContent = siteContent["contact"]["address"]
+paragraphs[6].textContent = siteContent["contact"]["phone"]
+paragraphs[7].textContent = siteContent["contact"]["email"]
+paragraphs[8].textContent = siteContent["footer"]["copyright"]
+
+// Button
+const button = document.querySelector("button");
+button.textContent = siteContent["cta"]["button"]
+
+// Append & Prepend
+//const extraText = document.createElement("h1")
+//extraText.textContent = "Extra Header"
+//extraText.style.color = "green"
+const extraText2 = document.createElement("h1")
+extraText2.textContent = "Extra Header2"
+extraText2.style.color = "green"
+const ctaClass = document.getElementsByClassName("cta")
+ctaClass.appendChild(extraText2)
