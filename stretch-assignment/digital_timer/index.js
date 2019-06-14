d1a3ab1a3e341cae271d688194b537cfc19c87a6
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-06-14 10:06:27 -0400

    Adjusted Buttons Beneath Timer And Styled Them

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -3,6 +3,7 @@ const body = document.getElementsByTagName("body")
 body[0].style.height = '36.5208rem'
 body[0].style.background = 'radial-gradient(circle, #fff, #ccc)'
 
+
 // Set Dashes to 0 for Start
 const secondsTens = document.getElementById("secondTens")
 secondsTens.textContent = 0
@@ -14,15 +15,27 @@ const msTens = document.getElementById("msTens")
 msTens.textContent = 0
 
 
-
 //Buttons
 const startButton = document.createElement("button")
 startButton.textContent = 'Start'
 body[0].appendChild(startButton)
 
-//const stopButton = document.createElement("button")
-//stopButton.textContent = 'Stop'
-
 const resetButton = document.createElement("button")
 resetButton.textContent = 'Reset'
 body[0].appendChild(resetButton)
+
+const button = document.getElementsByTagName("button")
+button[0].className = 'buttonClass'
+button[1].className = 'buttonClass'
+const buttonClass = document.getElementsByClassName('buttonClass')
+
+buttonClass[0].style.marginTop = '1.3rem'
+buttonClass[0].style.fontSize = '2rem'
+buttonClass[1].style.marginTop = '1.3rem'
+buttonClass[1].style.fontSize = '1.3rem'
+
+//const stopButton = document.createElement("button")
+//stopButton.textContent = 'Stop'
+
+// Move Buttons Underneath Timer
+body[0].style.flexDirection = 'column'
